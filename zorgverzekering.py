17e67549376d204ba3d0a1077a82cbc19aac3f43
folkert van heusden <mail@vanheusden.com>
2022-05-17 13:22:57 +0200

    ~choose ~secondopinion

--- zorgverzekering.py
+++ zorgverzekering.py
@@ -5,12 +5,16 @@
 # either install 'python3-paho-mqtt' or 'pip3 install paho-mqtt'
 
 import paho.mqtt.client as mqtt
+import random
 import threading
 import time
 
 mqtt_server  = '192.168.64.1'
 topic_prefix = 'GHBot/'
 channels     = ['nurdbottest', 'test']
+
+
+choices      = []
 
 def announce_commands(client):
     target_topic = f'{topic_prefix}to/bot/register'
@@ -19,9 +23,13 @@ def announce_commands(client):
     client.publish(target_topic, 'cmd=z|descr=Aankondiging van uitschakeling der neocortex for enige tijd.')
     client.publish(target_topic, 'cmd=test|descr=Werkt deze dingetje?')
     client.publish(target_topic, 'cmd=quit|descr=Afsluiten')
-    client.publish(target_topic, 'cmd=oens|descr=eh...')
+    client.publish(target_topic, 'cmd=oens|descr=Eh...')
+    client.publish(target_topic, 'cmd=choose|descr=Choose between comma seperated choices.')
+    client.publish(target_topic, 'cmd=secondopinion|descr=Eh...')
 
 def on_message(client, userdata, message):
+    global choices
+
     text = message.payload.decode('utf-8')
 
     topic = message.topic[len(topic_prefix):]
@@ -40,23 +48,34 @@ def on_message(client, userdata, message):
     if channel in channels:
         command = text[1:].split(' ')[0]
 
+        response_topic = f'{topic_prefix}to/irc/{channel}/privmsg'
+
         if command == 'zorgverzekering':
-            client.publish(f'{topic_prefix}to/irc/{channel}/privmsg', 'Het beste neem je een zorgverzekering die je ziektekosten afdekt.')
+            client.publish(response_topic, 'Het beste neem je een zorgverzekering die je ziektekosten afdekt.')
 
         elif command == 'z':
-            client.publish(f'{topic_prefix}to/irc/{channel}/privmsg', 'Truste!')
+            client.publish(response_topic, 'Truste!')
 
         elif command == 'test':
-            client.publish(f'{topic_prefix}to/irc/{channel}/privmsg', 'Deze dingetje werks.')
+            client.publish(response_topic, 'Deze dingetje werks.')
 
         elif command == 'quit':
-            client.publish(f'{topic_prefix}to/irc/{channel}/privmsg', 'NOOID!')
+            client.publish(response_topic, 'NOOID!')
 
         elif command == 'oens':
-            client.publish(f'{topic_prefix}to/irc/{channel}/privmsg', 'OENS OENS OENS')
+            client.publish(response_topic, 'OENS OENS OENS')
 
-        else:
-            print('No match for', text)
+        elif command == 'choose':
+            choices = ' '.join(text.split(' ')[1:]).split(',')
+
+            client.publish(response_topic, random.choice(choices))
+
+        elif command == 'secondopinion':
+            if len(choices) == 0:
+                client.publish(response_topic, 'Nothing to choose from')
+
+            else:
+                client.publish(response_topic, random.choice(choices))
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
