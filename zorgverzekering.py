68ccc96ba16331eb3800d7bcb760ca11e171a72b
NURDSpace <mail@vanheusden.com>
2022-05-19 16:50:49 +0200

    ~wau-temp

--- zorgverzekering.py
+++ zorgverzekering.py
@@ -26,6 +26,7 @@ def announce_commands(client):
     client.publish(target_topic, 'cmd=choose|descr=Choose between comma seperated choices.')
     client.publish(target_topic, 'cmd=secondopinion|descr=Eh...')
     client.publish(target_topic, 'cmd=spacehex|descr=Show current color of the space.')
+    client.publish(target_topic, 'cmd=wau-temp|descr=Temperature etc. in Wageningen.')
 
 def parse_to_rgb(json):
     if "value" in json:
@@ -41,6 +42,66 @@ def get_rgb():
     return [parse_to_rgb(
                 get_json("http://lichtsensor.dhcp.nurd.space/sensor/tcs34725_" + channel + "_channel"))
                 for channel in ["red", "green", "blue"]]
+
+def cmd_wau_temp(client, response_topic):
+    try:
+        r = requests.get('http://met.wur.nl/veenkampen/data/C_current.txt', timeout=10)
+
+        thermopage = r.content.decode('ascii').split()
+
+        currentline = thermopage[-1]
+
+        data = currentline.split(',')
+
+#        temp = str(round(float(data[2]),1))
+        temp = data[2]
+        humid = str(round(float(data[8]),1))
+        sunshine = data[9]
+        visibility = data[18]
+        precip = data[19]
+        pressure = str(float(data[21]))
+#        windspeed = str(round(float(data[22]),1))
+        windspeed = float(data[22])
+        windspeedstring = str(round(float(data[22]),1))
+        windangle = float(data[26])+11.25
+        winddir = ''
+
+        if windangle>0 and windangle<= 22.5: winddir = 'N'
+        if windangle>22.5 and windangle<= 45: winddir = 'NNE'
+        if windangle>45 and windangle<= 67.5: winddir = 'NE'
+        if windangle>67.5 and windangle<= 90: winddir = 'ENE'
+        if windangle>90 and windangle<= 112.5: winddir = 'E'
+        if windangle>112.5 and windangle<= 135: winddir = 'ESE'
+        if windangle>135 and windangle<= 157.5: winddir = 'SE'
+        if windangle>157.5 and windangle<= 180: winddir = 'SSE'
+        if windangle>180 and windangle<= 202.5: winddir = 'S'
+        if windangle>202.5 and windangle<= 225: winddir = 'SSW'
+        if windangle>225 and windangle<= 247.5: winddir = 'SW'
+        if windangle>247.5 and windangle<= 270: winddir = 'WSW'
+        if windangle>270 and windangle<= 292.5: winddir = 'W'
+        if windangle>292.5 and windangle<= 315: winddir = 'WNW'
+        if windangle>315 and windangle<= 337.5: winddir = 'NW'
+        if windangle>337.5 and windangle<= 360: winddir = 'NNW'
+        if windangle>360 and windangle<= 382.5: winddir = 'N'
+
+        if windspeed>0 and windspeed<= 0.3: windbeaufort = '0'
+        if windspeed>0.3 and windspeed<= 1.5: windbeaufort = '1'
+        if windspeed>1.5 and windspeed<= 3.3: windbeaufort = '2'
+        if windspeed>3.3 and windspeed<= 5.5: windbeaufort = '3'
+        if windspeed>5.5 and windspeed<= 7.9: windbeaufort = '4'
+        if windspeed>7.9 and windspeed<= 10.7: windbeaufort = '5'
+        if windspeed>10.7 and windspeed<= 13.8: windbeaufort = '6'
+        if windspeed>13.8 and windspeed<= 17.1: windbeaufort = '7'
+        if windspeed>17.1 and windspeed<= 20.7: windbeaufort = '8'
+        if windspeed>20.7 and windspeed<= 24.4: windbeaufort = '9'
+        if windspeed>24.4 and windspeed<= 28.4: windbeaufort = '10'
+        if windspeed>28.4 and windspeed<= 32.6: windbeaufort = '11'
+        if windspeed>32.6: windbeaufort = '12'
+
+        client.publish(response_topic, temp+' C, '+sunshine+' W/m2 sun, '+humid+'% humidity, '+windbeaufort+' bft ('+windspeedstring+' m/s) '+winddir+', '+precip+' mm precipitation, '+pressure+' kPa.')
+
+    except Exception as e:
+        client.publish(response_topic, f'Exception during "wau-temp": {e}, line number: {e.__traceback__.tb_lineno}')
 
 def on_message(client, userdata, message):
     global choices
@@ -90,6 +151,9 @@ def on_message(client, userdata, message):
             hexcolor = "#{:02x}{:02x}{:02x}".format(*get_rgb())
 
             client.publish(response_topic, 'Current hex color of zaal 1 is: ' + hexcolor)
+
+        elif command == 'wau-temp':
+            cmd_wau_temp(client, response_topic)
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
