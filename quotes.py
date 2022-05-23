8fb05a3668c8c0f1dac92a6854c16a123faad7ed
NURDSpace <mail@vanheusden.com>
2022-05-23 15:22:37 +0200

    fix for broken random quotes

--- quotes.py
+++ quotes.py
@@ -130,7 +130,7 @@ def on_message(client, userdata, message):
                     if '!' in nick:
                         nick = nick.split('!')[0]
 
-                    cur.execute('SELECT quote, nr FROM quotes WHERE channel=? AND (about_whom=? OR NOT INSTR(about_whom, ?) = 0 OR NOT INSTR(?, about_whom) = 0) AND _ROWID_ >= (abs(random()) % (SELECT max(_ROWID_) FROM quotes)) LIMIT 1', (channel, nick, nick, nick))
+                    cur.execute('SELECT quote, nr FROM quotes WHERE channel=? AND (about_whom=? OR about_whom like ? OR ? like printf("%%%s%%", about_whom)) ORDER BY RANDOM() LIMIT 1)', (channel, nick, nick, nick))
 
                     row = cur.fetchone()
 
