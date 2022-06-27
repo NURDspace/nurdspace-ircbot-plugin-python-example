ab364aa80c969efe6499255f051409a9dc0ffdc8
NURDSpace <mail@vanheusden.com>
2022-06-27 10:38:29 +0200

    rss announcers

--- rss.py
+++ rss.py
@@ -5,6 +5,7 @@
 # either install 'python3-paho-mqtt' or 'pip3 install paho-mqtt'
 # also: pip3 install feedparser or apt install python3-feedparser
 
+import calendar
 import feedparser
 import paho.mqtt.client as mqtt
 import random
@@ -15,11 +16,16 @@ import urllib.request
 
 mqtt_server  = 'mqtt.vm.nurd.space'
 topic_prefix = 'GHBot/'
-channels     = ['nurdbottest', 'nurds', 'test', 'nurdsbofh']
+channels     = ['nurdbottest', 'nurds', 'test', 'nurdsbofh']  # request rss feeds
 db_file      = 'rss.db'
 prefix       = '!'
+announce_in  = ['nurds']  # announcements of new entries
 
 con = sqlite3.connect(db_file)
+
+c = con.cursor()
+c.execute('PRAGMA journal_mode=wal')
+c.close()
 
 cur = con.cursor()
 
@@ -42,25 +48,92 @@ cur = con.cursor()
 
 feeds = dict()
 
-cur.execute('SELECT name, url, interval FROM feeds')
+cur.execute('SELECT name, url, interval, announce FROM feeds')
 
 for row in cur.fetchall():
     name = row[0]
 
-    feeds[name]             = dict()
-    feeds[name]['url']      = row[1]
-    feeds[name]['interval'] = row[2]
-    feeds[name]['last_poll'] = 0
-    feeds[name]['tree']     = None
+    feeds[name]              = dict()
+    feeds[name]['url']       = row[1]
+    feeds[name]['interval']  = row[2]
+    feeds[name]['last_poll'] = time.time()
+    feeds[name]['tree']      = None
+    feeds[name]['announce']  = row[3]
 
 cur.close()
+
+feed_lock = threading.Lock()
+
+def update_feed(client, name):
+    global announce_in
+    global feed_lock
+    global feeds
+    global topic_prefix
+
+    now = time.time()
+
+    if now - feeds[name]['last_poll'] < feeds[name]['interval'] and feeds[name]['tree'] != None:
+        return
+
+    print(f'Update content for {name} at {time.ctime()} from {feeds[name]["url"]}')
+
+    req = urllib.request.Request(
+            feeds[name]['url'], 
+            data=None, 
+            headers={
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+                }
+            )
+    
+    fh = urllib.request.urlopen(req)
+
+    tree = feedparser.parse(fh.read())
+
+    output = ''
+
+    for item in tree['items']:
+        if 'published_parsed' in item or 'updated_parsed' in item:
+            if 'updated_parsed' in item:
+                #print(item['updated_parsed'])
+                time_diff = calendar.timegm(item['updated_parsed']) - feeds[name]['last_poll']
+
+            else:
+                #print(item['published_parsed'])
+                time_diff = calendar.timegm(item['published_parsed']) - feeds[name]['last_poll']
+
+            #print(f'Oldness of item for {name}: {time_diff}')
+
+            if time_diff > 0:
+                if output != '':
+                    output += ' | '
+
+                new_text = f"Feed {name}: {item['title']} {item['link']}"
+
+                if len(output) + len(new_text) > 200:
+                    break
+
+                output += new_text
+
+    feed_lock.acquire()
+
+    feeds[name]['tree']       = tree
+    feeds[name]['item_index'] = 0
+    feeds[name]['last_poll']  = now
+
+    feed_lock.release()
+
+    if output != '' and feeds[name]['announce'] == 1:
+        for channel in announce_in:
+            response_topic = f'{topic_prefix}to/irc/{channel}/privmsg'
+
+            client.publish(response_topic, output)
 
 def announce_commands(client):
     global feeds
 
     target_topic = f'{topic_prefix}to/bot/register'
 
-    client.publish(target_topic, 'cmd=addrss|descr=Add an RSS feed: addrss <name> <url>')
+#    client.publish(target_topic, 'cmd=addrss|descr=Add an RSS feed: addrss <name> <url>')
     client.publish(target_topic, 'cmd=listrss|descr=List RSS feeds')
 
     for feed in feeds:
@@ -141,49 +214,35 @@ def on_message(client, userdata, message):
                 name = command.lower()
 
                 if name in feeds:
-                    try:
-                        now = time.time()
+                    #try:
+                    now = time.time()
 
-                        if feeds[name]['tree'] == None or now - feeds[name]['last_poll'] >= feeds[name]['interval']:
-                            print(f'Update content for {name}')
+                    if feeds[name]['tree'] == None or now - feeds[name]['last_poll'] >= feeds[name]['interval']:
+                        update_feed(client, name)
 
-                            req = urllib.request.Request(
-                                    feeds[name]['url'], 
-                                    data=None, 
-                                    headers={
-                                        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-                                        }
-                                    )
-                            
-                            fh = urllib.request.urlopen(req)
+                    n_items = len(feeds[name]['tree']['items']) if 'items' in feeds[name]['tree'] else 0
 
-                            feeds[name]['tree']       = feedparser.parse(fh.read())
-                            feeds[name]['item_index'] = 0
-                            feeds[name]['last_poll']  = now
+                    if n_items > 0:
+                        entry = feeds[name]
 
-                        n_items = len(feeds[name]['tree']['items']) if 'items' in feeds[name]['tree'] else 0
+                        nr    = entry['item_index']
+                        print(f'entry {nr} out of {n_items}')
 
-                        if n_items > 0:
-                            entry = feeds[name]
+                        text  = f"Feed {name}: {entry['tree']['items'][nr]['title']} {entry['tree']['items'][nr]['link']}"
 
-                            nr    = entry['item_index']
-                            print(f'entry {nr} out of {n_items}')
+                        nr += 1
+                        if nr >= n_items:
+                            nr = 0
 
-                            text  = f"Feed {name}: {entry['tree']['items'][nr]['title']} {entry['tree']['items'][nr]['link']}"
+                        feeds[name]['item_index'] = nr
 
-                            nr += 1
-                            if nr >= n_items:
-                                nr = 0
+                        client.publish(response_topic, text)
 
-                            feeds[name]['item_index'] = nr
+                    else:
+                        client.publish(response_topic, f'Feed "{name}" is empty? ({feeds[name]["url"]})')
 
-                            client.publish(response_topic, text)
-
-                        else:
-                            client.publish(response_topic, f'Feed "{name}" is empty? ({feeds[name]["url"]})')
-
-                    except Exception as e:
-                        client.publish(response_topic, f'Error while processing feed "{name}": {e}')
+                    #except Exception as e:
+                    #    client.publish(response_topic, f'Error while processing feed "{name}": {e}')
 
     except Exception as e:
         print(f'Error while processing {message}: {e}')
@@ -204,12 +263,26 @@ def announce_thread(client):
         except Exception as e:
             print(f'Failed to announce: {e}')
 
+def rss_poller(client):
+    while True:
+        for name in feeds:
+            try:
+                update_feed(client, name)
+
+            except Exception as e:
+                print(f'Cannot update feed {name}: {e}')
+
+        time.sleep(9)
+
 client = mqtt.Client()
 client.connect(mqtt_server, port=1883, keepalive=4, bind_address="")
 client.on_message = on_message
 client.on_connect = on_connect
 
-t = threading.Thread(target=announce_thread, args=(client,))
-t.start()
+t1 = threading.Thread(target=announce_thread, args=(client,))
+t1.start()
+
+t2 = threading.Thread(target=rss_poller, args=(client,))
+t2.start()
 
 client.loop_forever()
