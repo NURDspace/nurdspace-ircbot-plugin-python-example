67b7518e79aea5091e98881b3e2fb5e6d2340c03
NURDSpace <mail@vanheusden.com>
2022-07-24 08:29:19 +0200

    mpdsearch / mpdadd

--- mpd-plugin.py
+++ mpd-plugin.py
@@ -21,11 +21,15 @@ prefix       = '!'
 def announce_commands(client):
     target_topic = f'{topic_prefix}to/bot/register'
 
+    client.publish(target_topic, 'cmd=play|descr=Start playing.')
+    client.publish(target_topic, 'cmd=stop|descr=Stop playing.')
     client.publish(target_topic, 'cmd=next|descr=Skip to the next track.')
     client.publish(target_topic, 'cmd=pause|descr=Stops the music, or unstops it.')
     client.publish(target_topic, 'cmd=prev|descr=Skip to the previous track.')
     client.publish(target_topic, 'cmd=np|descr=What is playing right now?')
     client.publish(target_topic, 'cmd=clearpl|descr=Clear the current playlist.')
+    client.publish(target_topic, 'cmd=mpdsearch|descr=Search for a song, by title keywords.')
+    client.publish(target_topic, 'cmd=mpdadd|descr=Add a song(s) to the current play queue. Searches by title keyword(s).')
 
 def gen_song_name(song_meta):
     playing = ''
@@ -88,8 +92,8 @@ def on_message(client, userdata, message):
 
     command = text[1:].split(' ')[0]
 
-    if channel in channels and command in ['next', 'np', 'prev', 'pause', 'clearpl']:
-        response_topic = f'{topic_prefix}to/irc/{channel}/privmsg'
+    if channel in channels and command in ['next', 'np', 'prev', 'pause', 'clearpl', 'play', 'stop', 'mpdsearch', 'mpdadd']:
+        response_topic = f'{topic_prefix}to/irc/{channel}/notice'
 
         try:
             mpd_client = MPDClient()
@@ -111,10 +115,57 @@ def on_message(client, userdata, message):
 
                 client.publish(response_topic, f'Went back to the previous song')
 
+            elif command == 'play':
+                mpd_client.pause(0)
+
+                client.publish(response_topic, f'The music should be playing now')
+
+            elif command == 'stop':
+                mpd_client.pause(1)
+
+                client.publish(response_topic, f'The music should be stopped now')
+
+            elif command == 'mpdsearch' or command == 'mpdadd':
+                space = text.find(' ')
+
+                if space != -1:
+                    if command == 'mpdsearch':
+                        results = mpd_client.search('title', text[space + 1:])
+
+                        out = ''
+
+                        for result in results:
+                            if 'artist' in result and 'title' in result:
+                                if out != '':
+                                    out += ', '
+
+                                out += '\2' + result['artist'] + '\2: ' + result['title']
+
+                                if len(out) > 200:
+                                    break
+
+                        if out == '':
+                            client.publish(response_topic, 'No such song found')
+
+                        else:
+                            client.publish(response_topic, 'Results: ' + out)
+
+                    elif command == 'mpdadd':
+                        mpd_client.searchadd('title', text[space + 1:])
+
+                        client.publish(response_topic, 'Some song(s) might have been added to the play queue')
+
+                else:
+                    client.publish(response_topic, 'Parameter missing')
+
             elif command == 'pause':
                 mpd_client.pause()
 
-                client.publish(response_topic, f'The music is paused or unpaused: {status["state"]}')
+                if status["state"] == 'play':
+                    client.publish(response_topic, f'The music is unpaused')
+
+                else:
+                    client.publish(response_topic, f'The music is paused')
 
             elif command == 'clearpl':
                 mpd_client.clear()
@@ -122,23 +173,25 @@ def on_message(client, userdata, message):
 
                 client.publish(response_topic, f'The playlist has been cleared (and paused)')
 
-            elif command == 'np':
-                duration = float(status['duration'])
-                elapsed  = float(status['elapsed'])
+#            elif command == 'np':
+#                duration = float(status['duration'])
+#                elapsed  = float(status['elapsed'])
 
-                finished_percentage = elapsed * 100 / duration
+#                finished_percentage = elapsed * 100 / duration
 
-                time_left = duration - elapsed
+#                time_left = duration - elapsed
 
-                light_meters = 299792458 * time_left
+#                light_meters = 299792458 * time_left
 
-                client.publish(response_topic, f'Now playing: {playing} ({100 - finished_percentage:.2f}% left or {time_left:.2f} seconds or the time that light moves {light_meters / 1000:.2f} meters)')
+#                client.publish(response_topic, f'Now playing: {playing} ({100 - finished_percentage:.2f}% left or {time_left:.2f} seconds or the time that light moves {light_meters / 1000:.2f} meters)')
 
             mpd_client.close()
 
             mpd_client.disconnect()
 
         except Exception as e:
+            print(f'Exception: {e}, line number: {e.__traceback__.tb_lineno}')
+
             client.publish(response_topic, f'mpd: {e}')
 
 def on_connect(client, userdata, flags, rc):
