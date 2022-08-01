62133882758e65a71f1d046d64641c11236ef915
NURDSpace <mail@vanheusden.com>
2022-08-01 22:29:03 +0200

    ~reken

--- zorgverzekering.py
+++ zorgverzekering.py
@@ -45,6 +45,7 @@ def announce_commands(client):
     client.publish(target_topic, 'cmd=janee|descr=Voor levensvragen')
     client.publish(target_topic, 'cmd=regen|descr=Regenvoorspelling voor omgeving Wageningen')
     client.publish(target_topic, 'cmd=@|descr=What is the titel of the last URL posted')
+    client.publish(target_topic, 'cmd=reken|descr=Calculate a simple formula')
 
 def parse_to_rgb(json):
     if "value" in json:
@@ -215,6 +216,78 @@ def cmd_regen(client, response_topic):
     except:
         client.publish(response_topic, f'Exception during "regen": {e}, line number: {e.__traceback__.tb_lineno}')
 
+# from https://stackoverflow.com/questions/15197673/using-pythons-eval-vs-ast-literal-eval/68732605#68732605
+import ast, operator, math
+
+def safe_eval(s):
+    def checkmath(x, *args):
+        if x not in [x for x in dir(math) if not "__" in x]:
+            raise SyntaxError(f"Unknown func {x}()")
+        fun = getattr(math, x)
+        return fun(*args)
+
+    binOps = {
+        ast.Add: operator.add,
+        ast.Sub: operator.sub,
+        ast.Mult: operator.mul,
+        ast.Div: operator.truediv,
+        ast.Mod: operator.mod,
+        ast.Pow: operator.pow,
+        ast.Call: checkmath,
+        ast.BinOp: ast.BinOp,
+    }
+
+    unOps = {
+        ast.USub: operator.neg,
+        ast.UAdd: operator.pos,
+        ast.UnaryOp: ast.UnaryOp,
+    }
+
+    ops = tuple(binOps) + tuple(unOps)
+
+    tree = ast.parse(s, mode='eval')
+
+    def _eval(node):
+        if isinstance(node, ast.Expression):
+            return _eval(node.body)
+        elif isinstance(node, ast.Str):
+            return node.s
+        elif isinstance(node, ast.Num):
+            return node.value
+        elif isinstance(node, ast.Constant):
+            return node.value
+        elif isinstance(node, ast.BinOp):
+            if isinstance(node.left, ops):
+                left = _eval(node.left)
+            else:
+                left = node.left.value
+            if isinstance(node.right, ops):
+                right = _eval(node.right)
+            else:
+                right = node.right.value
+            return binOps[type(node.op)](left, right)
+        elif isinstance(node, ast.UnaryOp):
+            if isinstance(node.operand, ops):
+                operand = _eval(node.operand)
+            else:
+                operand = node.operand.value
+            return unOps[type(node.op)](operand)
+        elif isinstance(node, ast.Call):
+            args = [_eval(x) for x in node.args]
+            r = checkmath(node.func.id, *args)
+            return r
+        else:
+            raise SyntaxError(f"Bad syntax, {type(node)}")
+
+    return _eval(tree)
+
+def cmd_reken(client, response_topic, message):
+    try:
+        client.publish(response_topic, str(safe_eval(message)))
+
+    except Exception as e:
+        client.publish(response_topic, f'Exception during "reken": {e}, line number: {e.__traceback__.tb_lineno}')
+
 def on_message(client, userdata, message):
     global choices
     global last_urls
@@ -302,7 +375,6 @@ def on_message(client, userdata, message):
             cmd_wau_temp(client, response_topic)
 
         elif command == 'allot':
-            print(client, response_topic, value)
             cmd_allot(client, response_topic, value)
 
         elif command == 'rijnstreek':
@@ -316,6 +388,14 @@ def on_message(client, userdata, message):
 
         elif command == '@':
             cmd_at(client, response_topic)
+
+        elif command == 'reken':
+            space = text.find(' ')
+
+            if space != -1:
+                value = text[space:].strip()
+
+                cmd_reken(client, response_topic, value)
 
 def on_connect(client, userdata, flags, rc):
     if rc == 0:
