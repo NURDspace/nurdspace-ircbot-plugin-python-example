73acca63d3579cdf5afd5cc1bb3d3466380a6949
NURDSpace <mail@vanheusden.com>
2022-08-02 18:31:20 +0200

    dog facts

---
+++ dog.py
@@ -0,0 +1,94 @@
+#! /usr/bin/python3
+
+# by FvH, released under Apache License v2.0
+
+# either install 'python3-paho-mqtt' or 'pip3 install paho-mqtt'
+
+import paho.mqtt.client as mqtt
+import requests
+import threading
+import time
+
+mqtt_server  = 'mqtt.vm.nurd.space'   # TODO: hostname of MQTT server
+topic_prefix = 'GHBot/'  # leave this as is
+channels     = ['nurdbottest', 'nurds', 'nurdsbofh']  # TODO: channels to respond to
+prefix       = '!'  # !command, will be updated by ghbot
+
+def announce_commands(client):
+    target_topic = f'{topic_prefix}to/bot/register'
+
+    client.publish(target_topic, 'cmd=dogfact|descr=Dog facts')
+
+def get_json(url):
+    r = requests.get(url)
+
+    return r.json()
+
+def on_message(client, userdata, message):
+    global prefix
+
+    text = message.payload.decode('utf-8')
+
+    topic = message.topic[len(topic_prefix):]
+
+    if topic == 'from/bot/command' and text == 'register':
+        announce_commands(client)
+
+        return
+
+    if topic == 'from/bot/parameter/prefix':
+        prefix = text
+
+        return
+
+    if len(text) == 0:
+        return
+
+    parts   = topic.split('/')
+    channel = parts[2] if len(parts) >= 3 else 'nurds'  # default channel if can't be deduced
+    nick    = parts[3] if len(parts) >= 4 else 'jemoeder'  # default nick if it can't be deduced
+
+    if text[0] != prefix:
+        return
+
+    command = text[1:].split(' ')[0]
+
+    if channel in channels:
+        response_topic = f'{topic_prefix}to/irc/{channel}/privmsg'
+
+        if command == 'dogfact':
+            try:
+                j = get_json('https://dog-api.kinduff.com/api/facts')
+
+                fact = j['facts'][0]
+
+                client.publish(response_topic, fact)
+
+            except Exception as e:
+                client.publish(response_topic, f'Exception during "dogfact": {e}, line number: {e.__traceback__.tb_lineno}')
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        client.subscribe(f'{topic_prefix}from/irc/#')
+
+        client.subscribe(f'{topic_prefix}from/bot/command')
+
+def announce_thread(client):
+    while True:
+        try:
+            announce_commands(client)
+
+            time.sleep(4.1)
+
+        except Exception as e:
+            print(f'Failed to announce: {e}')
+
+client = mqtt.Client()
+client.connect(mqtt_server, port=1883, keepalive=4, bind_address="")
+client.on_message = on_message
+client.on_connect = on_connect
+
+t = threading.Thread(target=announce_thread, args=(client,))
+t.start()
+
+client.loop_forever()
