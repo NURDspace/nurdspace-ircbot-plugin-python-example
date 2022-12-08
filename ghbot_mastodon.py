5ae5d723608b1d297f0f94f5c65e28a8edb6b89f
NURDSpace <mail@vanheusden.com>
2022-12-08 16:05:06 +0100

    mastodon

---
+++ ghbot_mastodon.py
@@ -0,0 +1,124 @@
+#! /usr/bin/python3
+
+# by FvH, released under Apache License v2.0
+
+# either install 'python3-paho-mqtt' or 'pip3 install paho-mqtt'
+# pip3 install mastodon.py
+
+import paho.mqtt.client as mqtt
+import threading
+import time
+
+import socket
+import sys
+
+mqtt_server  = 'mqtt.vm.nurd.space'   # TODO: hostname of MQTT server
+topic_prefix = 'GHBot/'  # leave this as is
+channels     = ['nurdbottest', 'nurds', 'nurdsbofh']  # TODO: channels to respond to
+prefix       = '!'  # !command, will be updated by ghbot
+
+from io import StringIO
+from html.parser import HTMLParser
+
+class MLStripper(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.reset()
+        self.strict = False
+        self.convert_charrefs= True
+        self.text = StringIO()
+    def handle_data(self, d):
+        self.text.write(d)
+    def get_data(self):
+        return self.text.getvalue()
+
+def strip_tags(html):
+    s = MLStripper()
+    s.feed(html)
+    return s.get_data()
+
+from mastodon import Mastodon
+
+def announce_commands(client):
+    target_topic = f'{topic_prefix}to/bot/register'
+
+    client.publish(target_topic, 'cmd=mastodon|descr=Latests mastodon.nl timeline post')
+
+def on_message(client, userdata, message):
+    global prefix
+
+    text = message.payload.decode('utf-8')
+
+    topic = message.topic[len(topic_prefix):]
+
+    if topic == 'from/bot/command' and text == 'register':
+        announce_commands(client)
+
+        return
+
+    if topic == 'from/bot/parameter/prefix':
+        prefix = text
+
+        return
+
+    if len(text) == 0:
+        return
+
+    parts   = topic.split('/')
+    channel = parts[2] if len(parts) >= 3 else 'nurds'  # default channel if can't be deduced
+    nick    = parts[3] if len(parts) >= 4 else 'jemoeder'  # default nick if it can't be deduced
+
+    if text[0] != prefix:
+        return
+
+    command = text[1:].split(' ')[0]
+
+    if channel in channels or (len(channel) >= 1 and channel[0] == '\\'):
+        response_topic = f'{topic_prefix}to/irc/{channel}/privmsg'
+
+        if command == 'mastodon':
+            try:
+                mastodon = Mastodon(
+                    access_token = '/home/ghbot/nurdspace-ircbot-plugin-python-example/ghbot_usercred.secret',
+                    api_base_url = 'https://mastodon.nl'
+                )
+
+                m = mastodon.timeline_local()[0]
+
+                content = m['content']
+
+                user = '@' + m['account']['username'] + '@mastodon.nl'
+
+                posting = strip_tags(content)
+
+                del mastodon
+
+                client.publish(response_topic, f'[mastodon] \3{5}{user}\3: \x1d{posting}')
+
+            except Exception as e:
+                client.publish(response_topic, f'Failed to mastodon: {e}')
+
+def on_connect(client, userdata, flags, rc):
+    client.subscribe(f'{topic_prefix}from/irc/#')
+
+    client.subscribe(f'{topic_prefix}from/bot/command')
+
+def announce_thread(client):
+    while True:
+        try:
+            announce_commands(client)
+
+            time.sleep(4.1)
+
+        except Exception as e:
+            print(f'Failed to announce: {e}')
+
+client = mqtt.Client(f'{socket.gethostname()}_{sys.argv[0]}', clean_session=False)
+client.on_message = on_message
+client.on_connect = on_connect
+client.connect(mqtt_server, port=1883, keepalive=4, bind_address="")
+
+t = threading.Thread(target=announce_thread, args=(client,))
+t.start()
+
+client.loop_forever()
